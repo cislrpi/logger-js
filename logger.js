5ce54d4ade4cd504714c6f42f057aab7fd42043e
Ashwin Hamal <ash@ashwins-mbp.watson.ibm.com>
2016-06-24 14:34:18 -0400

    Logger updated.

--- logger.js
+++ logger.js
@@ -1,67 +1,113 @@
+const
+  fs = require('fs'),
+  _ = require('lodash'),
+  moment = require('moment'),
+  mongodb = require('mongodb'),
+  assert = require('assert'),
+  EventEmitter = require('events').EventEmitter;
 
-var logLevel = null;
+var defaults = {};
 
-function logExpression(expression, level) {
-  
-  if (logLevel == null) {
-    logLevel = 1;
-  }
-  
-  if (level > logLevel) return;
-  
-  else {
+function Logger(o) {
+  var logger = this;
+  var opts = this.opts = _.extend({}, defaults, o);
 
-    var date = new Date();
-    var year = insertZeroes(date.getFullYear(), 4);
-    var month = insertZeroes(date.getMonth() + 1, 2);
-    var day = insertZeroes(date.getDate(), 2);
-    var hour = insertZeroes(date.getHours(), 2);
-    var minute = insertZeroes(date.getMinutes(), 2);
-    var second = insertZeroes(date.getSeconds(), 2);
-    var msec = insertZeroes(date.getMilliseconds(), 3);
-    
-    if (typeof(expression) == 'string') {
-      console.log('[' + year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + '.' + msec + '] ' + expression);  
-    }
-    
-    else if (typeof(expression) == 'object') {
-      console.log('[' + year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + '.' + msec + '] ' + JSON.stringify(expression,null,2));  
-    }
+  if (opts.file)
+    this.logFile = fs.createWriteStream(opts.file, { flags: 'a' });
 
-    return;
+  if (opts.db) {
+    mongodb.MongoClient.connect(opts.db.url, (err, db) => {
+      if (err) {
+        logger.emit('db_error', err);
+        console.error('Error connecting to mongodb database: ' + opts.db.url);
+        return;
+      }
+      logger.dbColumn = db.collection('log');
+      logger.emit('db_connection', db);
+    });
   }
 }
 
-function insertZeroes(num, len) {
-  var text = '' + num;
-  while(text.length < len) {
-    text = '0' + text;
-  }
-  return text;
+Logger.prototype.__proto__ = EventEmitter.prototype;
+
+Logger.prototype.write = function(type, message, level) {
+  var o = this.opts;
+  if (
+    (o.min !== undefined && level < o.min) || 
+    (o.ax != undefined && level > o.max)
+  ) return;
+
+  if (typeof message == 'object')
+    message = JSON.stringify(message, null, 2);
+
+  var xpr = `[${moment().format('YYYY-MM-DD hh:mm:ss.SS')}] ${message}`;
+
+  if (
+    o.console && 
+    !(o.consoleMin !== undefined && level < o.consoleMin) &&
+    !(o.consoleMax !== undefined && level > o.consoleMax)
+  ) console.log(xpr);
+
+  if (
+    o.file && 
+    !(o.fileMin !== undefined && level < o.fileMin) &&
+    !(o.fileMax !== undefined && level > o.fileMax)
+  ) this.logFile.write(xpr + '\n');
+
+  if (
+    o.db && 
+    !(o.dbMin !== undefined && level < o.dbMin) &&
+    !(o.dbMax !== undefined && level > o.dbMax)
+  ) this.writeDb(type, message, level);
 }
 
-function setLogLevel(level) {
+Logger.prototype.writeDb = function(type, message) {
+  var logger = this;
+  var entry = {
+    message: message,
+    type: type
+  }
+  if (logger.dbColumn)
+     logger.dbColumn.insert(entry);
+  else
+    this.once('db_connection', () => {
+      logger.dbColumn.insert(entry);
+    });
+}
+
+Logger.prototype.log = function(message, level) {
+  this.write('log', message, level);
+}
+
+Logger.prototype.info = function(message, level) {
+  this.write('info', message, level);
+}
+
+Logger.prototype.warn = function(message, level) {
+  this.write('warn', message, level);
+}
+
+// Backwards compatibility + warning
+var logLevel = 1;
+var warned = false;
+
+var warn = (name) => {
+  if (warned) return;
+  console.warn(`Warning: "${name}" has been deprecated, please use "new Logger()".`)
+  warned = true;
+}
+Logger.setLogLevel = (level) => {
+  warn('setLogLevel');
   logLevel = level;
 }
-
-//test();
-
-function test() {
-
-  logExpression('important', 1);
-  logExpression('less important', 2);
-
-  var JSONstuff = {
-    "msg": "Here is an important message.",
-    "msg2": "Here is a less important message."
-  };
-
-  logExpression(JSONstuff, 1);
-  logExpression(JSONstuff, 2);
+Logger.logExpression = (xpr, level) => {
+  if (level > logLevel) return;
+  xpr = (typeof xpr == 'object') ? JSON.stringify(xpr, null, 2) : xpr;
+  console.log(`[${moment().format('YYYY-MM-DD hh:mm:ss.SS')}] ${xpr}`);
+}
+Logger.insertZeroes = (num, len) => {
+  warn('insertZeroes');
+  return Array(num).join('0') + text;
 }
 
-exports = module.exports = {
-    logExpression: logExpression,
-    setLogLevel: setLogLevel,
-    insertZeroes: insertZeroes
-}
+module.exports = Logger;
